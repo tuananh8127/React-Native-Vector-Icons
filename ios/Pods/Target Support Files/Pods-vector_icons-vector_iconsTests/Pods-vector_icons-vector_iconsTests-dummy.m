505ad270c2a757b610f28babe0cbcb1ed5e9e681
mr anh <votuananh8127@gmail.com>
2022-05-06 03:38:22 +0700

    ios

---
+++ ios/Pods/Target Support Files/Pods-vector_icons-vector_iconsTests/Pods-vector_icons-vector_iconsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_vector_icons_vector_iconsTests : NSObject
+@end
+@implementation PodsDummy_Pods_vector_icons_vector_iconsTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-vector_icons/Pods-vector_icons-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_vector_icons : NSObject
+@end
+@implementation PodsDummy_Pods_vector_icons
+@end
